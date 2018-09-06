ce88815c0a2907b3e21f9a62bbcafdb0862fb084
Michal Czyz <michal.kacper.czyz@gmail.com>
2018-09-06 11:40:26 +0200

    Moving old ideas to legacy dir.

---
+++ SVEngine/legacy/SVModule.py
@@ -0,0 +1,43 @@
+# MIT License
+#
+# Copyright (c) 2018 Michal Czyz
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#!/usr/bin/env python
+import logging
+
+
+class SVModule:
+
+    def __init__(self):
+        self.name = None
+        self.parameters = None
+        self.ports = None
+
+    def init_args(self, name, parameters, ports):
+        self.name = name
+        self.parameters = parameters
+        self.ports = ports
+
+    def debug_display(self):
+        class_name = str(self.__class__.__name__)
+        logging.debug(class_name + ' : Name : ' + str(self.name))
+        logging.debug(class_name + ' : Parameters : ' + str(self.parameters))
+        logging.debug(class_name + ' : Ports : ' + str(self.ports))
